cd571e10ae6b092b3e24fb14306455073703c47c
luxigaung <823074995@qq.com>
2015-06-26 18:14:35 +0800

    添加搜索框

--- MyApp/MyApp/FourthViewController.swift
+++ MyApp/MyApp/FourthViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class FourthViewController: BaseViewController {
+class FourthViewController: BaseViewController,UIWebViewDelegate,UISearchBarDelegate {
 
+    @IBOutlet var searchB: UISearchBar!
+    @IBOutlet var mainLiftBar: UIBarButtonItem!
+    @IBOutlet var mainrightBar: UIBarButtonItem!
+    @IBOutlet var mainReplyBar: UIBarButtonItem!
+    @IBOutlet var mainStopBar: UIBarButtonItem!
+    @IBOutlet var mainProgress: UIProgressView!
+    @IBOutlet var mainWebView: UIWebView!
+    var time:NSTimer!//计时器
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mainProgress.progress = 0
+        
     }
-
+    //前进按钮
+    @IBAction func liftAction(sender: AnyObject) {
+        mainWebView.goBack()
+    }
+    //后退按钮
+    @IBAction func rightAction(sender: AnyObject) {
+        mainWebView.goForward()
+    }
+    //刷新按钮
+    @IBAction func replyAction(sender: AnyObject) {
+        mainWebView.reload()
+    }
+    //停止加载按钮
+    @IBAction func stopAction(sender: AnyObject) {
+        mainWebView.stopLoading()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
